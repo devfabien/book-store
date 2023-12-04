7156591e38c0fcbd7856604e0164a1374b91f013
devfabien <ish001fabi@gmail.com>
2023-12-04 13:25:07 +0200

    feat: adding the function to get a single book

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { BOOKS } from 'src/mocks/books.mock';
 
 @Injectable()
@@ -8,6 +8,17 @@ export class BooksService {
   getBooks(): Promise<any> {
     return new Promise((resolve) => {
       resolve(this.books);
+    });
+  }
+
+  getBook(bookId): Promise<any> {
+    const id = Number(bookId);
+    return new Promise((resolve) => {
+      const book = this.books.find((book) => book.id === id);
+      if (!book) {
+        throw new HttpException('Book does not exist!', 404);
+      }
+      resolve(book);
     });
   }
 }
